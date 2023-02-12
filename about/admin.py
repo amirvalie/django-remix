625c6d3566040b910ed5ffd4a9fb005ccd6617ab
amirvalie <hosseinvalie@gmail.com>
2023-02-12 18:44:16 +0330

    Added models to the admin file in about app

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import (
+    About,
+    Contact,
+)
 # Register your models here.
+
+class AboutAdmin(admin.ModelAdmin):
+    pass
+
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(About,AboutAdmin)
+admin.site.register(Contact,ContactAdmin)
