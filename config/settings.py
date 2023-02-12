3f2b41ece89835f8d2da8a3a5f1c97195e885aca
amirvalie <hosseinvalie@gmail.com>
2023-02-12 13:37:21 +0330

    Added STATIC settings to the setting file

--- config/settings.py
+++ config/settings.py
@@ -115,9 +115,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+     BASE_DIR / "static",
+]
+
+MEDIA_URL='/media/'
+MEDIA_ROOT= BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
