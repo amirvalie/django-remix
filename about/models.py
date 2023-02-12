74bd20a9d14ad2ba2d6e87fb50b6b5c12d834724
amirvalie <hosseinvalie@gmail.com>
2023-02-12 11:01:14 +0330

    Added models to to model file in about app

--- about/models.py
+++ about/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class About(models.Model):
+    description=models.TextField(
+        verbose_name='توضحیات'
+    )
+    picture=models.ImageField(
+        upload_to='/',
+        verbose_name='عکس پروفایل'
+    )
+    
+class Contact(models.Model):
+    name=models.CharField(
+        max_length=20,
+        verbose_name='نام'
+    )
+    email=models.EmailField()
+    title=models.CharField(
+        max_length=250,
+        verbose_name='عنوان'
+    )
+    description=models.TextField(
+        verbose_name='توضیحات',
+    )
