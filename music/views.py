29df85364f2c6b53e6d268b66baf75b81eb18932
amirvalie <hosseinvalie@gmail.com>
2023-02-16 16:08:47 +0330

    Added view for all model in music app

--- music/views.py
+++ music/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+# from django.db import Q
 from django.views.generic import (
     ListView,
     DetailView,
@@ -10,14 +11,48 @@ from .models import (
     Banner,
     ComingSoon,
 )
-# Create your views here.
 
-class Home(ListView):
-    model=Track.objects.remix()
-    template_name='templates/music/index.html'
+class Index(ListView):
+    queryset=Track.objects.remix()
+    template_name='music/index.html'
+    context_object_name='tracks'
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        context['podcasts']=Track.objects.podcast()
+        context['best_songs']=Track.objects.best_songs()
+        context['atists']=Artist.objects.active()
+        context['banners']=Banner.objects.active()
+        context['coming_soon']=ComingSoon.objects.active()
+        return context        
+
+class DetailTrack(DetailView):
+    model=Track
+    template_name='music/track_detail.html'
     context_object_name='track'
     def get_context_data(self,**kwargs):
         context=super().get_context_data(**kwargs)
-        context['podcast']=Track.objects.podcast()
-        context['best_songs']=Track.objects.best_songs()
-        context['atists']=Artist.objects.all()
+        context['related_tracks']=Track.objects.filter(
+            Q(category=self.get_object().category) | 
+            Q(description__contains=self.get_object().description)
+        )
+        return context
+
+class AllPublicTrackOfCategory(DetailView):
+    queryset=Category.objects.active()
+    template_name='music/category_tracks.html'
+    context_object_name='tracks'
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data()
+        category=self.get_object()
+        context['tracks']=category.tracks.active()
+        return context
+        
+class ListOfArtist(ListView):
+    queryset=Artist.objects.active()
+    template_name='music/list_of_artists.html'
+    context_object_name='artists'
+
+class DetailArtist(DetailView):
+    queryset=Artist.objects.active()
+    template_name='music/detail_artist.html'
+    context_object_name='artist'
