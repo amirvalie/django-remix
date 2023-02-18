77a9efe8a7bf044ec35ec0580147d43841cc4f63
amirvalie <hosseinvalie@gmail.com>
2023-02-18 15:48:16 +0330

    update DB

--- music/urls.py
+++ music/urls.py
@@ -6,7 +6,8 @@ app_name='music'
 urlpatterns=[
     path('index/',views.Index.as_view(),name='index'),
     path('track/<slug:slug>/',views.DetailTrack.as_view(),name='detail_track'),
-    path('index',views.AllPublicTrackOfCategory.as_view(),name='index'),
-    path('index',views.ListOfArtist.as_view(),name='index'),
+    path('category/<slug:slug>/tracks/',views.AllPublicTrackOfCategory.as_view(),name='tracks_of_category'),
+    path('artists/',views.ListOfArtist.as_view(),name='artists'),
+    path('index',views.DetailArtist.as_view(),name='index'),
     path('index',views.DetailArtist.as_view(),name='index'),
 ]
