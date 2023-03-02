a49e4430ab13214707209c691ab214780c20a2b9
amirvalie <hosseinvalie@gmail.com>
2023-03-02 19:41:22 +0330

    checkout to 1f47d19 commit

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('music.urls')),
-    path('', include('about.urls')),
+    # path('', include('about.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
