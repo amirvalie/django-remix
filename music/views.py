22f915d8d3c22ad1b0eb0bf8f00c4d8a231a4628
amirvalie <hosseinvalie@gmail.com>
2023-03-02 09:14:53 +0330

    Added get_object method to DetailTrack to return only active objects and changed AllPublicTrackOfCategory to a formal name CategoryList and inherit from ListView to access category itself

--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,get_object_or_404
 from django.db.models import Q
-
 from django.views.generic import (
     ListView,
     DetailView,
@@ -27,9 +26,14 @@ class Index(ListView):
         return context        
 
 class DetailTrack(DetailView):
-    model=Track
     template_name='remix/detail-track.html'
     context_object_name='track'
+    
+    def get_object(self):
+        slug=self.kwargs.get('slug')
+        track=get_object_or_404(Track.objects.active(),slug=slug)
+        return track
+
     def get_context_data(self,**kwargs):
         context=super().get_context_data(**kwargs)
         context['related_tracks']=Track.objects.filter(
@@ -38,29 +42,34 @@ class DetailTrack(DetailView):
         )
         return context
 
-class AllPublicTrackOfCategory(DetailView):
-    queryset=Category.objects.filter(status=True)
-    template_name='music/category_tracks.html'
-    context_object_name='tracks'
-    def get_context_data(self,**kwargs):
-        context=super().get_context_data()
-        category=self.get_object()
-        context['tracks']=category.tracks.active()
-        return context
-        
+class CategoryList(ListView):
+	paginate_by = 5
+	template_name = 'remix/category_list.html'
+
+	def get_queryset(self):
+		global category
+		slug = self.kwargs.get('slug')
+		category = get_object_or_404(Category.objects.active(), slug=slug)
+		return category.tracks.active()
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['category'] = category
+		return context
+
+
 class ListOfArtist(ListView):
     queryset=Artist.objects.filter(status=True)
-    template_name='remix/list_of_artists.html'
+    template_name='remix/archive-bio.html'
     context_object_name='artists'
 
 class DetailArtist(DetailView):
     queryset=Artist.objects.filter(status=True)
-    template_name='remix/detail_artist.html'
+    template_name='remix/single-bio.html'
     context_object_name='artist'
 
 class SearchTrackOrArtist(ListView):
     template_name='remix/search_result.html'
-    context_object_name='results'
     def get_queryset(self):
         query=self.request.GET.get('q',None)
         result=Track.objects.filter(
