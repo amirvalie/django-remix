adeeefc97add80ca8135fe9dc2772a461df1c80a
amirvalie <hosseinvalie@gmail.com>
2023-03-05 16:41:48 +0330

    Added middelware file for save ip address in database to know the number of view for every Track object

--- config/settings.py
+++ config/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'music.middelware.SaveIpAddressMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
