d61679b453fd4a7ba9cca8e139a354f4af40042d
amirvalie <hosseinvalie@gmail.com>
2023-06-26 18:09:19 +0330

    Added Postqresql database

--- config/settings.py
+++ config/settings.py
@@ -91,12 +91,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':env('DB_Name'), 
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD_USER'),
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
