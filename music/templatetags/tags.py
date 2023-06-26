6b2b5d5e4f333e91989da2eac3c36b00534605f2
amirvalie <hosseinvalie@gmail.com>
2023-06-26 18:09:58 +0330

    limit number of item in home page

--- music/templatetags/tags.py
+++ music/templatetags/tags.py
@@ -18,6 +18,7 @@ from extentions.utils import jalali_converter
 
 register = template.Library()
 
+
 @register.inclusion_tag("remix/footer.html",takes_context=True)
 def footer(context):
 
@@ -27,15 +28,18 @@ def footer(context):
         'about_me':context.get('about_me'),
     }
 
+
 @register.filter
 def convert_to_jalali(time):
     return jalali_converter(time)
+
 
 @register.inclusion_tag("remix/top-menue.html",takes_context=True)
 def top_menue(context):
     return {
         'about_me':context.get('about_me')
     }
+
 
 @register.inclusion_tag("remix/navbar/navbar.html",takes_context=True)
 def navbar(context):
@@ -46,6 +50,7 @@ def navbar(context):
         'about_me':context.get('about_me')
     }
 
+
 @register.inclusion_tag("remix/site_control/sidbar.html",takes_context=True)
 def sidbar(context):
     return{
@@ -53,11 +58,12 @@ def sidbar(context):
         'dynamic_sidbars':Sidebar.objects.filter(status=True)[:4],
     }
 
+
 @register.simple_tag
 def call_method(category_obj,filter):
     try:
         method=getattr(category_obj, 'find_tracks')
-        return method(filter)
+        return method(filter)[:12]
     except AttributeError:
         return None
 
@@ -65,6 +71,7 @@ def call_method(category_obj,filter):
 @register.simple_tag
 def return_class_name(obj):
     return obj.__class__.__name__
+
 
 @register.simple_tag
 def subcategories(obj):
