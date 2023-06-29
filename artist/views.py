d8578e6645bc29be63929c0220e08fb3186461af
amirvalie <hosseinvalie@gmail.com>
2023-06-29 11:15:25 +0330

    Added related tracks for each artist

--- artist/views.py
+++ artist/views.py
@@ -26,3 +26,8 @@ class DetailArtist(DetailView):
     queryset=Artist.objects.active()
     template_name='remix/artist/artist-detail.html'
     context_object_name='artist'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['related_tracks'] = self.get_object().tracks.active()
+        return context
